bdbc37d7bc20a784eae198b978cb4450893a01fd
fullgukbap <2hanjunbum6@gmail.com>
2024-03-14 15:24:46 +0900

    ✨ Game core 개발 완료

---
+++ game.go
@@ -0,0 +1,29 @@
+package main
+
+type Player struct {
+	ID int `json:"id"`
+	X  int `json:"x"`
+	Y  int `json:"y"`
+}
+
+type Game struct {
+	Players map[int]*Player
+}
+
+func NewGame() *Game {
+	return &Game{Players: make(map[int]*Player)}
+}
+
+func (g *Game) AddPlayer(p *Player) {
+	g.Players[p.ID] = p
+}
+
+func (g *Game) RemovePlayer(playerID int) {
+	delete(g.Players, playerID)
+}
+
+func (g *Game) MovePlayer(playerID, deltaX, deltaY int) {
+	player := g.Players[playerID]
+	player.X += deltaX
+	player.Y += deltaY
+}
