39bdd20f6fc16fedf007e0f5d4a0ed64d75c67ee
fullgukbap <2hanjunbum6@gmail.com>
2024-03-21 14:55:49 +0900

    update: login 갱신
    클라이언트랑 disconnect 통신이 안되길레 connection 안에 넣어서 했더니 됌 그래서 코드를 이렇게 변경함!
    그리고 event도 io.on이 아니라 socket.on으로 작성해야 한다.

--- server.js
+++ server.js
@@ -4,23 +4,25 @@ const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(server, {path: '/socket.io'});
 
 // server의 정보 
 let players = {};
 
 io.on("connection", (socket) => {
+    console.log("New player: ", socket.id)
     players[socket.id] = {x: 0, y: 0};
     socket.emit("welcome", players)
         
-    io.on("event", (data) => {
+    socket.on("event", (data) => {
+        console.log("event detected | player : ", socket.id);
         players[socket.id] = data;
     });
-});
 
-
-io.on("disconnect", (socket) => {
-    delete players[socket.id];
+    socket.on("disconnect", () => {
+        console.log("logout player: ", socket.id);
+        delete players[socket.id];
+    });
 });
 
 setInterval(() => {
