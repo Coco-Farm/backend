9a51bb0e9c68c3a82772971cfca22171dea90401
fullgukbap <2hanjunbum6@gmail.com>
2024-05-09 16:31:02 +0900

    feat: player move 기능 구현

--- internal/core/model/player.go
+++ internal/core/model/player.go
@@ -1,11 +1,34 @@
 package model
 
+import "errors"
+
+var (
+	ErrInvalidDirection = errors.New("invalid direditon input")
+)
+
 // Player 구조체는 본 게임의 사용자를 표현합니다.
 type Player struct {
 	ID string `json:"id"`
 	X  int    `json:"x"`
 	Y  int    `json:"y"`
 
-	// PlayerSpeed is ignored by this package.
-	PlayerSpeed int `json:"-"`
+	// Speed is ignored by this package.
+	Speed int `json:"-"`
+}
+
+func (p *Player) Move(direction string) error {
+	switch direction {
+	case "up":
+		p.Y += 1 * p.Speed
+	case "down":
+		p.Y -= 1 * p.Speed
+	case "left":
+		p.X -= 1 * p.Speed
+	case "right":
+		p.X += 1 * p.Speed
+	default:
+		return ErrInvalidDirection
+	}
+
+	return nil
 }
